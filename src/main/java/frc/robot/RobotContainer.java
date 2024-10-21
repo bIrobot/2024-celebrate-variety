17cbcd1cf5b9bb306e2a559d7e94250af255ac8e
KevinCBoyle <kevin@boyle.bz>
2024-10-21 15:18:45 -0400

    full stop and pick up

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -5,6 +5,7 @@ import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.RunCommand;
 import frc.robot.Constants.OIConstants;
+import frc.robot.LimelightHelpers.LimelightTarget_Fiducial;
 import frc.robot.subsystems.ArmSubsystem;
 import frc.robot.subsystems.DriveSubsystem;
 import frc.robot.subsystems.IngestSubsystem;
@@ -22,6 +23,8 @@ public class RobotContainer {
     double lastTX = 100;
     double lastTY;
     long when;
+    long whenstop;
+    boolean fullstop = false;
 
     public RobotContainer() {
         configureSwerveDrive();
@@ -32,11 +35,21 @@ public class RobotContainer {
 
         if (driverController.getStartButtonPressed() || driverController.getBackButtonPressed()){
             seeking = ! seeking;
+            fullstop = false;
+            LimelightHelpers.setLEDMode_ForceOn("limelight");
             if (driverController.getBackButton()) {
                 lastTX = -100;
             } else { 
                 lastTX = 100;
             }
+        }
+        if (fullstop) {
+            ingestModule.startIngesting();
+            if (System.nanoTime()-whenstop > 3000000000L) {
+                seeking = false;
+                fullstop = false;
+            }
+            return;
         }
         if (! seeking) {
             shouldLeftArmChangeState();
@@ -51,16 +64,24 @@ public class RobotContainer {
             TA=LimelightHelpers.getTA("limelight");
             TX=LimelightHelpers.getTX("limelight");
             TY=LimelightHelpers.getTY("limelight");
+            //if seeing target
             if (TA != 0) { 
+                // if seeing target x
                 if (TX != 0) {
+                    //remember last x position and time
                     lastTX=TX;
                     when = System.nanoTime();
-                }
+                } 
+                //if seeing target y
                 if (TY != 0) {
+                    //remember last y position and time
                     lastTY = TY;
                     when = System.nanoTime();
                 }
+                
+                //if target is dead ahead
                 if (Math.abs(TX)<5) {
+                    //if target is too close 
                     if (TA > 5.3) {
                         System.out.println("Stop " + TA);
                         robotDrive.drive(0, 0, 0, false, true);
@@ -68,25 +89,39 @@ public class RobotContainer {
                         System.out.println("Go forward " + TA);
                         robotDrive.drive(0.1, 0, -TX/40, false, true);
                     }
+                //is target on left
                 } else if (TX<0) {
                     System.out.println("Rotate left slow"); 
                     robotDrive.drive(0.1, 0, 0.1, false, true);
+                // target is on right
                 } else {
                     System.out.println("Right Slow");
                     robotDrive.drive(0.1, 0, -0.1, false, true);
                 }
+            // not seeing target
             } else {
                 System.out.println("Dont see");
+                // if last time was deadahead 
                 if (Math.abs(lastTX)<5) {
-                    if (lastTY < 0 && System.nanoTime()-when < 2000000000) {
+                    //if last time was in bottom of view and less than 2 seconds ago 
+                    if (lastTY < 0 && System.nanoTime()-when < 750000000) {
                         System.out.println("Keep going");
                     } else {
+                        //come to full stop
                         robotDrive.drive(0, 0, 0, false, true);
+                        System.out.println("Full Stop");
+                        LimelightHelpers.setLEDMode_ForceOff("limelight");
+                        fullstop = true;
+                        whenstop = System.nanoTime();
+                        ingestModule.startIngesting();
                     }
                 } else {
+                    //if last time was to the left x 
                     if (lastTX<0) {
+                        //rotate left
                         robotDrive.drive(0, 0, 0.2, false, true);
                     } else {
+                        //roate right
                         robotDrive.drive(0, 0, -0.2, false, true);
                     }
                 }
